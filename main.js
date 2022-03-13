3d653eed4ccb261010cce286a4b22f20f826867a
Hannah Bottum <hannah.bottum82@journalism.cuny.edu>
2022-03-13 16:06:37 -0400

    final commit

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-// silence
+//no notes
+const select = document.querySelector(".filter");
+
+select.addEventListener("change", function (e){
+    const chosen_immigrants = e.target.value;
+    const currently_visible = document.querySelectorAll(".item.active");
+    const make_visible = document.querySelectorAll(`.item.${chosen_immigrants}`);
+    
+    currently_visible.forEach(function (immigrants){
+        immigrants.classList.remove("active");
+    });
+
+    make_visible.forEach(function(immigrants){
+        immigrants.classList.add("active")
+    });
+});
